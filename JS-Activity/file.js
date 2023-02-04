d73e68b2207c9ca652fcc9d8967a85d44698034a
dex h <114088497+dexiesuser@users.noreply.github.com>
2023-02-04 08:40:57 +0800

    Add files via upload

---
+++ JS-Activity/file.js
@@ -0,0 +1,5 @@
+var age = 14;
+var maxAge = 200;
+var numPerDay = 2;
+var totalRequired =(numPerDay * 365) * (maxAge - age);
+document.write("You will need " + totalRequired + " to last you until the ripe old age of " + maxAge);
